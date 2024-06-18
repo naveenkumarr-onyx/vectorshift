37c4a850d3c497b1673fbf188d9cd701253fc547
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-06-18 17:44:36 +0530

    ui

---
+++ frontend/src/nodes/textNode.js
@@ -0,0 +1,34 @@
+// textNode.js
+
+import { useState } from "react";
+import { Handle, Position } from "reactflow";
+
+export const TextNode = ({ id, data }) => {
+  const [currText, setCurrText] = useState(data?.text || "{{input}}");
+
+  const handleTextChange = (e) => {
+    setCurrText(e.target.value);
+  };
+
+  return (
+    <div
+      style={{
+        width: 200,
+        height: 90,
+        border: "1px solid black",
+        padding: "5px",
+      }}
+    >
+      <div>
+        <span>Text</span>
+      </div>
+      <div>
+        <label>
+          Text:
+          <input type="text" value={currText} onChange={handleTextChange} />
+        </label>
+      </div>
+      <Handle type="source" position={Position.Right} id={`${id}-output`} />
+    </div>
+  );
+};
